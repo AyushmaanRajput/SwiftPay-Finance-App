ce95dfc1828307d5cd26605a422757381b7c2200
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2023-09-27 20:15:36 +0530

    test

--- src/components/admin/AllTransactions.jsx
+++ src/components/admin/AllTransactions.jsx
@@ -1,73 +1,81 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
-import styled from 'styled-components'
-import { Button } from '../Buttons'
+import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import styled from "styled-components";
+import { Button } from "../Buttons";
 
 export const AllTransactions = () => {
-  const [pages,setPages] = useState(1)
-  const [totalPages,setTotalPages] = useState(0)
-  const [limit]= useState(10)
-  const [data,setData] = useState([])
-  useEffect(()=>{
-    fetchData(limit,pages)
-  },[limit,pages])
-  const fetchData = async (limit,pages) => {
+  const [pages, setPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit] = useState(10);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchData(limit, pages);
+  }, [limit, pages]);
+  const fetchData = async (limit, pages) => {
     try {
-      let res = await axios.get(`https://warlike-current.onrender.com/transactions?_page=${pages}&_limit=${limit}`)
+      let res = await axios.get(
+        `https://warlike-current.onrender.com/transactions?_page=${pages}&_limit=${limit}`
+      );
       const total = res.headers.get("X-Total-Count");
-      setTotalPages(Math.ceil(total/limit));
-      setData(res.data)
+      setTotalPages(Math.ceil(total / limit));
+      setData(res.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   const handleClick = (page) => {
-    setPages(page)
-  }
+    setPages(page);
+  };
 
   // console.log(data)
   return (
     <MAINSECTION>
-      <h3 className='heading'>AllTransactions</h3>
-      <br /><br />
-      
-      {
-        data?.map((item)=>{
-           return <div className='transaction-card' key={item.id}>
+      <h3 className="heading">AllTransactions</h3>
+      <br />
+      <br />
+
+      {data?.map((item) => {
+        return (
+          <div className="transaction-card" key={item.id}>
             <p>Id: {item.id}</p>
-           <p>From: {item.from}</p>
-           <p>Payment: {`${Math.abs(+item.amount)}`}</p>
-           <p>To: {item.to}</p>
-           <p>Message: {item.message}</p>
-           <p>Date: {item.date.split("T")[0]} at {item.date.split("T")[1]}</p>
-         </div>
-        })
-      }
-      {
-        totalPages>1 && new Array(totalPages).fill(0).map((el,i)=>{
-          return <Button key={i} onClick={()=>handleClick(i+1)}>{i+1}</Button>
-        })
-      }
+            <p>From: {item.from}</p>
+            <p>Payment: {`${Math.abs(+item.amount)}`}</p>
+            <p>To: {item.to}</p>
+            <p>Message: {item.message}</p>
+            <p>
+              Date: {item.date.split("T")[0]} at {item.date.split("T")[1]}
+            </p>
+          </div>
+        );
+      })}
+      {totalPages > 1 &&
+        new Array(totalPages).fill(0).map((el, i) => {
+          return (
+            <Button key={i} onClick={() => handleClick(i + 1)}>
+              {i + 1}
+            </Button>
+          );
+        })}
     </MAINSECTION>
-  )
-}
+  );
+};
 
 const MAINSECTION = styled.main`
-  font-family: 'Courier New', Courier, monospace;
+  font-family: "Courier New", Courier, monospace;
   color: white;
   text-align: left;
 
-  .heading{
+  .heading {
     text-align: center;
-    color :white;
+    color: white;
   }
-  p{
+  p {
     font-size: medium;
   }
-  .transaction-card{
-    border: 2px  solid white;
-    padding:10px;
-    margin-bottom: .8rem;
+  .transaction-card {
+    border: 2px solid white;
+    padding: 10px;
+    margin-bottom: 0.8rem;
   }
-` 
+`;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
-import { legacy_createStore, applyMiddleware } from "redux";
+import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 import { reducer as authReducer } from "./authReducer/reducer";
+import { reducer as transactionsReducer } from "./admin/transactionsReducer/reducer";
 import thunk from "redux-thunk";
 
-export const store = legacy_createStore(authReducer, applyMiddleware(thunk));
+const rootReducer = combineReducers({ authReducer, transactionsReducer });
+
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));

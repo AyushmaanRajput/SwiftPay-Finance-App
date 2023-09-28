dc73ff1691ab020d0ec945d2d563c41c5f9aea7c
yashuvaishnav <yashurishu22@gmail.com>
2023-09-28 19:17:07 +0530

    day-3

--- src/redux/admin/transactionsReducer/reducer.js
+++ src/redux/admin/transactionsReducer/reducer.js
@@ -1,9 +1,22 @@
+import { GET_TRANSACTION_FAILURE, GET_TRANSACTION_REQUEST, GET_TRANSACTION_SUCCESS } from "./actionTypes";
+
 const initState = {
   transactions: [],
+  isLoading : false,
+  isError : false
 };
 
-export const reducer = (state = initState, action) => {
-  switch (action.type) {
+export const reducer = (state = initState, {type,payload}) => {
+  switch (type) {
+    case GET_TRANSACTION_REQUEST : {
+      return {...state,isLoading:true,isError:false}
+    }
+    case GET_TRANSACTION_SUCCESS : {
+      return {...state,isLoading:false,isError:false,transactions:payload}
+    }
+    case GET_TRANSACTION_FAILURE : {
+      return {...state,isError:true,isLoading:false}
+    }
     default:
       return state;
   }

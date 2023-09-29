560da8f93be57e2412ed4a03a3801bf65ce5d30d
yashuvaishnav <yashurishu22@gmail.com>
2023-09-29 11:34:00 +0530

    Day-3

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 import { reducer as authReducer } from "./authReducer/reducer";
 import { reducer as transactionsReducer } from "./admin/transactionsReducer/reducer";
+import { reducer as userTransactionsReducer} from "./userTransactions/reducer"
 import thunk from "redux-thunk";
 
-const rootReducer = combineReducers({ authReducer, transactionsReducer });
+const rootReducer = combineReducers({ authReducer, transactionsReducer,userTransactionsReducer });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
---
+++ src/redux/userTransactions/reducer.js
@@ -0,0 +1,34 @@
+import { GET_ADMIN_TRANSACTION_FAILURE, GET_ADMIN_TRANSACTION_REQUEST, GET_ADMIN_TRANSACTION_SUCCESS, GET_USERS_TRANSACTION_FAILURE, GET_USERS_TRANSACTION_REQUEST, GET_USERS_TRANSACTION_SUCCESS } from "./actionTypes"
+
+
+export const initialState = {
+    isLoading:false,
+    isError:false,
+    usersTransactions : [],
+    adminTransactions : []
+}
+
+export const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case GET_ADMIN_TRANSACTION_REQUEST : {
+        return{...state,isLoading:true,isError:false}
+    }
+    case GET_ADMIN_TRANSACTION_SUCCESS : {
+        return{...state,isLoading:false,isError:false,adminTransactions:payload}
+    }
+    case GET_ADMIN_TRANSACTION_FAILURE : {
+        return{...state,isLoading:true,isError:true}
+    }
+    case GET_USERS_TRANSACTION_REQUEST : {
+        return{...state,isLoading:true,isError:false}
+    }
+    case GET_USERS_TRANSACTION_SUCCESS : {
+        return{...state,isLoading:false,isError:false,usersTransactions:payload}
+    }
+    case GET_USERS_TRANSACTION_FAILURE : {
+        return{...state,isLoading:false,isError:true}
+    }
+  default:
+    return state
+  }
+}

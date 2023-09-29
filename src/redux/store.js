dcc2eec439282506da81cd27b9d8306b31b30ba5
yashuvaishnav <yashurishu22@gmail.com>
2023-09-29 23:01:23 +0530

    day-4

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,17 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
 import { reducer as authReducer } from "./authReducer/reducer";
 import { reducer as transactionsReducer } from "./admin/transactionsReducer/reducer";
-import { reducer as userTransactionsReducer} from "./userTransactions/reducer"
+import { reducer as userTransactionsReducer} from "./user/userTransactions/reducer"
 import { reducer as usersReducer } from "./user/usersReducer/reducer";
+import { reducer as userQueryReducer } from "./user/userQuery/reducer";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   authReducer,
   transactionsReducer,
   usersReducer,
-  userTransactionsReducer
+  userTransactionsReducer,
+  userQueryReducer
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));

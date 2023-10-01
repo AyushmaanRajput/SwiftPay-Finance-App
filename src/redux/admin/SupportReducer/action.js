a32f957d636a41d093c8a43ea401aa00ee4aad03
yashuvaishnav <yashurishu22@gmail.com>
2023-10-01 12:31:40 +0530

    Day-5

--- src/redux/admin/SupportReducer/action.js
+++ src/redux/admin/SupportReducer/action.js
@@ -19,7 +19,7 @@ export const adminTransactionsData = (dispatch) => {
 export const postQuery = (queryObj) => (dispatch) => {
     dispatch({type:POST_QUERY_REQUEST})
     axios
-    .post(`https://mock-api-finpay.onrender.com/supports`,queryObj)
+    .post(`http://localhost:8080/support`,queryObj)
     .then((res)=>{
       console.log(res)
       dispatch({type:POST_QUERY_SUCCESS})
@@ -30,7 +30,7 @@ export const postQuery = (queryObj) => (dispatch) => {
 export const supportData = (dispatch) => {
     dispatch({type:GET_SUPPORT_REQUEST})
     axios
-      .get('https://warlike-current.onrender.com/supports')
+      .get('http://localhost:8080/support')
       .then((res) => {
         dispatch({type:GET_SUPPORT_SUCCESS,payload:res.data})
       })
